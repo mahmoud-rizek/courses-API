cfae550f3516ee7f1d2a5faf52953b7b7feec9a7
unknown <peoplemany676@gmail.com>
2023-02-15 04:18:45 +0200

    add custo_pagination, custom_fillter

--- coursers/urls.py
+++ coursers/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import  CoursesApi, CoursesApiDetail
+
 app_name = 'coursers'
 
 
 
 urlpatterns = [
     # path("api/", course_api),
-    path("api/", CoursesApi.as_view()),
-    path("api/<int:pk>", CoursesApiDetail.as_view())
+    path("api/", CoursesApi.as_view(), name='course_list'),
+    path("api/<int:pk>", CoursesApiDetail.as_view(), name='course_detail')
 ]
--- coursers/views.py
+++ coursers/views.py
@@ -1,16 +1,34 @@
 from django.shortcuts import render
-from .models import Content, CourseReviews, Courses, Instructor, CourseReviews
-from .serializer import CourseSerializer, InstractorSerializer, ContentSerializer
 from rest_framework.response import Response
-from rest_framework.decorators import api_view 
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
+import django_filters.rest_framework 
+# from rest_framework.decorators import api_view 
+
+from .models import Courses
+from .serializer import CourseSerializer
+from .paginations import CoursePaginations
+from .fillters import CoursesFillter
+
 
 # @api_view('GET')
 # def course_api(request):
 #     queryset = Courses.objects.all()
 #     data = CourseSerializer(queryset).data
 #     return Response({'allProduct':data})
+
+
+
+class CoursesApi(generics.ListCreateAPIView):
+    queryset = Courses.objects.all()
+    serializer_class = CourseSerializer
+    pagination_class = CoursePaginations
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'price']
+    filterset_class = CoursesFillter
+    # search_fields = ['name']
+    # permission_classes = [IsAuthenticated]
+
 
 
 class CoursesApiDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -20,15 +38,3 @@ class CoursesApiDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 
-
-
-class CoursesApi(generics.ListCreateAPIView):
-    queryset = Courses
-    serializer_class = CourseSerializer
-    filter_backends = [DjangoFilterBackend]
-    # pagination_class = paginationStyleName
-    # filter_backends = [DjangoFilterBackend]
-    # filterset_fields = ['name', 'price']
-    # filterset_class = fillterName
-    # search_fields = ['name']
-    # permission_classes = [IsAuthenticated]
